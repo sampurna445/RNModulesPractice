7ee2beaa8f6b00c8d8dd3e5d01b6987f953fd76f
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-11-01 14:01:59 +0000

    Extracted  code to separate component and refactored accordingly

---
+++ ios/RCTCalendarModule.h
@@ -0,0 +1,16 @@
+//
+//  RCTCalendarModule.h
+//  RNModulesPractice
+//
+//  Created by itc on 01/11/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTCalendarModule : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/RCTCalendarModule.m
@@ -0,0 +1,12 @@
+//
+//  RCTCalendarModule.m
+//  RNModulesPractice
+//
+//  Created by itc on 01/11/2023.
+//
+
+#import "RCTCalendarModule.h"
+
+@implementation RCTCalendarModule
+
+@end
